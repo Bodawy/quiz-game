cd50a03f350bfad35625e7185935f183ed2d15e4
Bodawy <140910059+Bodawy@users.noreply.github.com>
2024-06-13 01:50:56 +0300

    2nd video

--- quiz_game.py
+++ quiz_game.py
@@ -1,21 +1,36 @@
-# Modules
+# Module
 import pyfiglet
 import termcolor
 import pyttsx3
 import winsound
 
-# Style
+# Speak function
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Title 
 print(termcolor.colored(pyfiglet.figlet_format("Welcome to"), color='yellow'))
 print(termcolor.colored(pyfiglet.figlet_format("Quiz Game"), color='green'))
 
+# Ask to play?
 playing = input("Do you want to play? ")
 
 if playing.lower() != "yes":
+    speak("Okay bye!")
+    print("Okay bye!")
     quit()
 
+speak("Okay! Let's play :)")
 print("Okay! Let's play :)")
+
 score = 0
 
+speak("What does CPU stand for? ")
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print(termcolor.colored('Correct!', color='green'))
@@ -25,6 +40,7 @@ else:
     print(termcolor.colored('Incorrect!', color='red'))
     winsound.PlaySound('incorrect.wav', winsound.SND_FILENAME) 
 
+speak("What does GPU stand for? ")
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
     print(termcolor.colored('Correct!', color='green'))
@@ -34,6 +50,7 @@ else:
     print(termcolor.colored('Incorrect!', color='red'))
     winsound.PlaySound('incorrect.wav', winsound.SND_FILENAME) 
 
+speak("What does RAM stand for? ")
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print(termcolor.colored('Correct!', color='green'))
@@ -43,6 +60,7 @@ else:
     print(termcolor.colored('Incorrect!', color='red'))
     winsound.PlaySound('incorrect.wav', winsound.SND_FILENAME) 
 
+speak("What does PSU stand for? ")
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
     print(termcolor.colored('Correct!', color='green'))
@@ -52,5 +70,15 @@ else:
     print(termcolor.colored('Incorrect!', color='red'))
     winsound.PlaySound('incorrect.wav', winsound.SND_FILENAME) 
 
-print("You got " + str(score) + " questions correct!")
-print("You got " + str((score / 4) * 100) + "%.")
+# Score bar
+if score < 2 :
+    print('#'*20)
+    print(termcolor.colored(f"You got " + str(score) + " questions correct!", color="red"))
+    print(termcolor.colored(f"You got " + str((score / 4) * 100) + "%.", color='red'))
+    print('#'*20)
+
+elif score >= 2 :
+    print('#'*20)
+    print(termcolor.colored(f"You got " + str(score) + " questions correct!", color="green"))
+    print(termcolor.colored(f"You got " + str((score / 4) * 100) + "%.", color='green'))
+    print('#'*20)    
